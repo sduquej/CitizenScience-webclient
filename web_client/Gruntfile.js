6a48629606360cd0705c4c1da77c08781497157b
Sebastián Duque <sduquej@gmail.com>
2015-03-01 12:00:29 +0000

    added json files to build

--- web_client/Gruntfile.js
+++ web_client/Gruntfile.js
@@ -361,6 +361,7 @@ module.exports = function (grunt) {
           src: [
             '*.{ico,png,txt}',
             '.htaccess',
+            '*.json',
             '*.html',
             'views/{,*/}*.html',
             'images/{,*/}*.{webp}',
