a881cea18ea9c893440d4075fc8ee067b9e2de42
Sebastián Duque <sduquej@gmail.com>
2015-03-01 12:09:29 +0000

    properly copying data/form_fields.json

--- web_client/Gruntfile.js
+++ web_client/Gruntfile.js
@@ -361,7 +361,7 @@ module.exports = function (grunt) {
           src: [
             '*.{ico,png,txt}',
             '.htaccess',
-            '*.json',
+            'data/*.json',
             '*.html',
             'views/{,*/}*.html',
             'images/{,*/}*.{webp}',
